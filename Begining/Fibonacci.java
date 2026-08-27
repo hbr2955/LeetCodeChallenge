96d80f235b023231e2d636a3c95712d6d381c982
hbr2955 <hrushikeshrokde@gmail.com>
2026-08-27 09:57:11 +0530

    #3 Fibonacci number improved

--- Begining/Fibonacci.java
+++ Begining/Fibonacci.java
@@ -6,8 +6,12 @@ public class Fibonacci {
     * EXAMPLE: 0 and 1 is given --> third term = 0+1 i.e. 1
     * Fourth term  1 + 1 =2*/
 
-    int fibo = 0;
 
+    /* Using recursive call but its not best solution as has approximately:
+
+        Time: O(2^n)
+        Space: O(n) due to the recursion call stack.
+    */
     public int fib(int n)
     {
        if(n==0)
@@ -22,6 +26,24 @@ public class Fibonacci {
            return fib(n-1)+fib(n-2); // Recursive call
        }
 
+    }
+
+
+    public int fib2(int n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        int previous = 0;
+        int current = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int next = previous + current;
+            previous = current;
+            current = next;
+        }
+
+        return current;
     }
 
     public static void main(String[] args) {
